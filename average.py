f76a3ddefa539c2f912ec6e708314e86a61256c7
ShrimayiS <112842665+ShrimayiS@users.noreply.github.com>
2022-12-22 15:05:16 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,6 +1,17 @@
 import pandas as pd
-
+import numpy as np
 #read csv file
+
+df = pd.read_csv("iris.csv")
+
 #find the average of petal width
+
+m = np.mean(df["pw"]);
+
 #round off to 2 decimal places
+
+m = round(m,2)
+
 #print only average
+
+print(m)
